f038e4fa332e153c396a4279497e820fefabf338
DESKTOP-CFOMFKS\User <marius.pasiulevicius@gmail.com>
2020-12-02 19:26:55 +0200

    Add project files.

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Domain;
+
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                    new Value { ID = 1, Name = "Value 01" },
+                    new Value { ID = 2, Name = "Value 02" },
+                    new Value { ID = 3, Name = "Value 03" }
+                );
+        }
+    }
+}
---
+++ Persistence/Migrations/20201202164245_SeedValues.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class SeedValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Values",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 1, "Value 01" });
+
+            migrationBuilder.InsertData(
+                table: "Values",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 2, "Value 02" });
+
+            migrationBuilder.InsertData(
+                table: "Values",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 3, "Value 03" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Values",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Values",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Values",
+                keyColumn: "ID",
+                keyValue: 3);
+        }
+    }
+}
