1cffcea7a7f744b5a6b9548c80156c4385b972cf
DESKTOP-CFOMFKS\User <marius.pasiulevicius@gmail.com>
2020-12-17 19:43:45 +0200

    axios logic added

--- my-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ my-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import { Grid } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import ActivityDetails from '../details/ActivityDetails';
@@ -12,12 +12,26 @@ interface IProps {
     editMode: boolean;
     setEditMode: (editMode: boolean) => void;
     setSelectedActivity: (activity: IActivity | null) => void;
-    createActivity:(activity:IActivity)=>void;
-    editActivity:(activity:IActivity)=>void;
-    deleteActivity:(id: string) => void;
+    createActivity: (activity: IActivity) => void;
+    editActivity: (activity: IActivity) => void;
+    deleteActivity: (e:SyntheticEvent<HTMLButtonElement> ,id: string) => void;
+    submitting: boolean;
+    target: string;
 }
 
-const ActivityDashboard: React.FC<IProps> = ({ activities, selectActivity, selectedActivity, editMode, setEditMode, setSelectedActivity,createActivity, editActivity, deleteActivity}) => {
+const ActivityDashboard: React.FC<IProps> = ({
+    activities,
+    selectActivity,
+    selectedActivity,
+    editMode,
+    setEditMode,
+    setSelectedActivity,
+    createActivity,
+    editActivity,
+    deleteActivity,
+    submitting,
+    target
+}) => {
     return (
         <div>
             <Grid>
@@ -25,7 +39,10 @@ const ActivityDashboard: React.FC<IProps> = ({ activities, selectActivity, selec
                     <ActivityList
                         activities={activities}
                         selectActivity={selectActivity}
-                        deleteActivity={deleteActivity} />
+                        deleteActivity={deleteActivity}
+                        submitting={submitting}
+                        target={target}
+                        />
                 </Grid.Column>
                 <Grid.Column width={6}>
                     {selectedActivity && !editMode &&
@@ -33,13 +50,14 @@ const ActivityDashboard: React.FC<IProps> = ({ activities, selectActivity, selec
                             activity={selectedActivity}
                             setEditMode={setEditMode}
                             setSelectedActivity={setSelectedActivity} />}
-                    {editMode && <ActivityForm 
-                            key = {(selectedActivity && selectedActivity.id) || (0)}
-                            setEditMode={setEditMode} 
-                            activity={selectedActivity!}
-                            createActivity={createActivity}
-                            editActivity={editActivity}
-                            />}
+                    {editMode && <ActivityForm
+                        key={(selectedActivity && selectedActivity.id) || (0)}
+                        setEditMode={setEditMode}
+                        activity={selectedActivity!}
+                        createActivity={createActivity}
+                        editActivity={editActivity}
+                        submitting={submitting}
+                    />}
                 </Grid.Column>
             </Grid>
         </div>
