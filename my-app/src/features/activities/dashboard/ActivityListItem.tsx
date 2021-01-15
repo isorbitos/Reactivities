8df4a2f06d875638af1a56d56b69d4b751b7f50f
DESKTOP-CFOMFKS\User <marius.pasiulevicius@gmail.com>
2021-01-15 15:21:47 +0200

    photo upload client side added

--- my-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ my-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -7,24 +7,28 @@ import ActivityListItemAttendees from './ActivityListItemAttendees';
 
 
 const ActivityListitem: React.FC<{ activity: IActivity }> = ({ activity }) => {
-    const host = activity.attendees.filter(x=> x.isHost)[0];
+    const host = activity.attendees.filter(x => x.isHost)[0];
     return (
         <Segment.Group>
             <Segment>
                 <Item.Group>
                     <Item>
-                        <Item.Image size='tiny' circular src={host.image || '/assets/user.png'} />
+                        <Item.Image
+                            size='tiny'
+                            circular
+                            src={host.image || '/assets/user.png'}
+                            style={{marginBottom : 3}} />
                         <Item.Content>
                             <Item.Header as={Link} to={`/activities/${activity.id}`}>{activity.title}</Item.Header>
                             <Item.Description>
-                                Hosted by {host.displayName}
-                        </Item.Description>
+                                Hosted by<Link to={`/profile/${host.username}`}> {host.displayName}</Link> 
+                            </Item.Description>
                             {activity.isHost &&
                                 <Item.Description>
                                     <Label basic color='orange' content='You are hosting this activity' />
                                 </Item.Description>
                             }
-                             {activity.isGoing && !activity.isHost &&
+                            {activity.isGoing && !activity.isHost &&
                                 <Item.Description>
                                     <Label basic color='green' content='You are going to this activity' />
                                 </Item.Description>
